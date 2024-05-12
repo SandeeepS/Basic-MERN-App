701b42167fab0b9da3faea77c8a605c776519c6b
SandeeepS <sandeepsanthosh3303@gmail.com>
2024-05-12 06:19:10 +0530

    added auth middleware

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -6,7 +6,22 @@ import generateToken from "../utils/generateToken.js";
 //route POST /api/users/auth
 //@access public
 const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Auth user" });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
 });
 
 //@desc Register a new user
@@ -28,39 +43,73 @@ const resgisterUser = asyncHandler(async (req, res) => {
     password,
   });
 
-  if(user){
-    generateToken(res,user._id)
-     res.status(201).json({
-       _id: user._id,
-       name: user.name,
-       email: user.email
-     })
-  }else{
+  if (user) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
     res.status(400);
-    throw new Error('Invalid user data')
+    throw new Error("Invalid user data");
   }
-
 });
 
 //@desc Logout user
 //route POST /api/users/Logout
 //@access public
 const logoutUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Logout user" });
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: "Logged out successfully" });
 });
 
 //@desc Get user profile
 //route POST /api/users/profile
 //@access private
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "User profile" });
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 //@desc Update user profile
 //route POST /api/users/profile
 //@access private
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: " Update User profile" });
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 export {
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,20 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { notFound,errorHandler } from './middleware/errorMiddleware.js';
-import connectDb from './config/db.js';
+import express from "express";
+import dotenv from "dotenv";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDb from "./config/db.js";
 dotenv.config();
-import userRoutes from './Routes/userRoutes.js'
+import cookieParser from "cookie-parser";
+import userRoutes from "./Routes/userRoutes.js";
 const port = process.env.PORT || 5000;
 const app = express();
 app.use(express());
-app.use(express.urlencoded({extended:true}))
-connectDb(); 
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
+connectDb();
 
-app.use('/api/users',userRoutes);
-app.use(notFound)
+app.use("/api/users", userRoutes);
+app.use(notFound);
 app.use(errorHandler);
 
-
-app.get('/',(req,res) => res.send('Server is working'));
-app.listen(port,() => console.log(`server started on port ${port}`));
+app.get("/", (req, res) => res.send("Server is working"));
+app.listen(port, () => console.log(`server started on port ${port}`));
